65b01fd900d2d753cd19fdbabe1056f31505c972
Gabriel Lima <121446890+GabrielLima5@users.noreply.github.com>
2023-01-25 17:39:37 -0300

    upload dos arquivos

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const select = document.querySelector("#operators");
+const nextStageButton = document.querySelector("#send-option");
+const calcButton = document.querySelector("#calc");
+const inputs = document.querySelector(".inputs");
+const resultField = document.querySelector(".resultado");
+const firstInput = document.querySelector("#first-n");
+const secondInput = document.querySelector("#second-n");
+const firstLabel = document.querySelector("#first-label");
+const secondLabel = document.querySelector("#second-label");
+inputs.style.display = "none";
+
+select.addEventListener("change", (e) => {
+  resultField.innerHTML = "";
+  firstInput.value = "";
+  secondInput.value = "";
+  var operator = select.value;
+  if (operator === "exponentiation" || operator === "square") {
+    secondInput.style.display = "none";
+    firstLabel.innerHTML = "Número";
+    secondLabel.style.display = "none";
+  } else {
+    secondInput.style.display = "flex";
+    firstLabel.innerHTML = "Primeiro número";
+    secondLabel.style.display = "flex";
+  }
+});
+
+nextStageButton.addEventListener("click", (e) => {
+  inputs.style.display = "flex";
+  nextStageButton.style.display = "none";
+});
+
+calcButton.addEventListener("click", (e) => {
+  var n1 = parseFloat(firstInput.value);
+  var n2 = parseFloat(secondInput.value);
+  let operator = select.value;
+  if (operator === "addition") {
+    let result = n1 + n2;
+    resultField.innerHTML = result;
+  } else if (operator === "subtraction") {
+    let result = n1 - n2;
+    resultField.innerHTML = result;
+  } else if (operator === "multiplication") {
+    let result = n1 * n2;
+    resultField.innerHTML = result;
+  } else if (operator === "division") {
+    let result = n1 / n2;
+    resultField.innerHTML = result;
+  } else if (operator === "exponentiation") {
+    let result = n1 * n1;
+    resultField.innerHTML = result;
+  } else if (operator === "square") {
+    let result = n1 ** 0.5;
+    resultField.innerHTML = result;
+  }
+});
